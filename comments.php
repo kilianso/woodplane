ce7b7f0e57211141f11894ed58c78c6f754930ea
Kilian So <hello@kilianso.com>
2022-07-31 15:36:35 +0200

    use new comments setup

--- comments.php
+++ comments.php
@@ -1,85 +1,16 @@
 <?php
-/**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Woodplane
- */
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
-if ( post_password_required() ) {
-	return;
+wp_list_comments( array(
+    'short_ping' => true,
+    'type' => 'comment',
+    'callback' => 'betterComments',
+));
+
+function betterComments($comment, $args, $depth) {
+    $context = Timber::context();
+    $context['comment'] = $comment;
+    $context["commentReplyArgs"] = array('reply_text' => "Reply", 'depth' => 1, 'max_depth' => 2);
+    Timber::render( 'partials/Comment/Comment.twig', $context );
 }
-?>
 
-<section id="comments" class="post_comments">
-	<div class="container is-narrow">
-		<div class="row">
-			<div class="col-sm-12">
-				<?php
-				// You can start editing here -- including this comment!
-				if ( have_comments() ) : ?>
-				<h2 class="comments-title">
-					<?php
-					$comment_count = get_comments_number();
-					if ( '1' === $comment_count ) {
-						printf(
-							/* translators: 1: title. */
-							esc_html__($comment_count . ' Comment', 'wdpln' )
-						);
-					} else {
-						printf( // WPCS: XSS OK.
-							/* translators: 1: comment count number, 2: title. */
-							esc_html__($comment_count . ' Comments', 'wdpln' )
-							// number_format_i18n( $comment_count )
-						);
-					}
-					?>
-				</h2><!-- .comments-title -->
-
-				<ol class="comment-list">
-					<?php
-					wp_list_comments( array(
-						'style'      => 'ul',
-						'short_ping' => true,
-						'callback' => [wdpln_theme()->Package->Comments, 'betterComments'],
-						'type' => 'comment',
-						'args' => false
-					) );
-					?>
-				</ol><!-- .comment-list -->
-
-				<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :  ?>
-					<div class="col-sm-12 text-center">
-						<?php previous_comments_link( 'More comments', 0 ); ?>
-					</div>
-        		<?php endif; // check for comment navigation ?>
-
-				<?php
-					// If comments are closed and there are comments, let's leave a little note, shall we?
-					if ( ! comments_open() ) : ?>
-						<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'wdpln' ); ?></p>
-						<?php
-					endif;
-
-				endif; // Check for have_comments().
-
-				$comments_args = array(
-				   'submit_button' => '<div class="comment-form-submit text-center margin--small">
-		            								<input name="%1$s" type="submit" id="%2$s" class="%3$s btn btn-outline btn-rounded" value="%4$s" />
-	        										</div>'
-				);
-				comment_form($comments_args);
-				?>
-			</div>
-		</div>
-	<div>
-</section><!-- #comments -->
+// The wrapper to output comments properly can be found in components/Comments/Comments.twig and will be included in single.twig
